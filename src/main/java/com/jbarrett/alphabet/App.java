84a5cd2e92102cea42c234b1ce2577f2eb609491
Jason Barrett <jason95@vt.edu>
2019-06-15 13:11:30 -0400

    Initialize

---
+++ src/main/java/com/jbarrett/alphabet/App.java
@@ -0,0 +1,14 @@
+package com.mkyong.hashing;
+
+/**
+ * Hello world!
+ *
+ */
+public class App
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+        
+    }
+}
